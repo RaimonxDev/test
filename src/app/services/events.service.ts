1c6cc304bac6f809c949f2a26ebd17582a2c1568
Ramon Martinez <ramonmartinezcordero@gmail.com>
2022-08-20 07:01:09 -0400

    fixed url

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,43 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {Event} from 'src/app/Event'
-import {Invite} from 'src/app/Invite'
+import { Event } from 'src/app/Event'
+import { Invite } from 'src/app/Invite'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  private apiUrlEvent='http://localhost:5000/events';
-  private apiUrlInvite='http://localhost:5000/invites';
+  private apiUrlEvent = 'http://localhost:5000/eAvents';
+  private apiUrlInvite = 'http://localhost:5000/invites';
 
 
   constructor(private http: HttpClient) { }
 
 
-getEventsByUserId(userID: number): Observable<Event []> {
-  return this.http.get<Event[]>(`http://localhost:5000/events?userID=${userID}`) 
-}
-
-getAllEvents(): Observable<Event []> {
-  return this.http.get<Event[]>(`http://localhost:5000/events`) 
+  getEventsByUserId(userID: number): Observable<Event[]> {
+    return this.http.get<Event[]>(`${this.apiUrlEvent}?userID=${userID}`)
   }
 
-postNewEvent(event : Event):Observable<undefined>{
-  return this.http.post<undefined>(this.apiUrlEvent, event)
-}
+  getAllEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(`${this.apiUrlEvent}`)
+  }
 
-deleteEvent(id: number): Observable<Event> {
-  return this.http.delete<Event>(`http://localhost:5000/events/${id}`)
-}
+  postNewEvent(event: Event): Observable<undefined> {
+    return this.http.post<undefined>(this.apiUrlEvent, event)
+  }
 
-editEvent(id: number, event: Event): Observable<Event> {
-  return this.http.put<Event>(`http://localhost:5000/events/${id}`, event)
-}
+  deleteEvent(id: number): Observable<Event> {
+    return this.http.delete<Event>(`${this.apiUrlEvent}/${id}`)
+  }
 
-postNewInvite(invite : Invite):Observable<undefined>{
-  return this.http.post<undefined>(this.apiUrlInvite, invite)
-}
+  editEvent(id: number, event: Event): Observable<Event> {
+    return this.http.put<Event>(`${this.apiUrlEvent}/${id}`, event)
+  }
+
+  postNewInvite(invite: Invite): Observable<undefined> {
+    return this.http.post<undefined>(this.apiUrlInvite, invite)
+  }
 
 }
